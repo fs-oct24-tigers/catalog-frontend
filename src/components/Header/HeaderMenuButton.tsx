bce694e3af02497164c67d521c268e587f0f6830
Andrii Yanchuk <horeca288@gmail.com>
2025-01-13 12:52:35 +0200

    Create adaptive header and Mobile Menu

---
+++ src/components/Header/HeaderMenuButton.tsx
@@ -0,0 +1,9 @@
+import { Menu } from 'lucide-react';
+
+export const HeaderMenuButton = () => {
+  return (
+    <div className="flex size-16 items-center justify-center border-x border-gray-700 hover:bg-lineGray">
+      <Menu size={16} />
+    </div>
+  );
+};
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const Main: React.FC<Props> = ({ children }) => {
-  return <main className="max-w-xl mx-auto">{children}</main>;
+  return <main className="max-w-xl mx-auto relative">{children}</main>;
 };
 
 export default Main;
