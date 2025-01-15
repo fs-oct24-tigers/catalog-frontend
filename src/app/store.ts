82fb8942251371efc6c9926f442f61ab5d97a8b1
Igor <4430704@gmail.com>
2025-01-15 12:08:05 +0200

    add redux persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,31 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit';
-
 import { cartSlice } from '../features/cart';
 import { favoritesSlice } from '@/features/favorites';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart', 'favorites'],
+};
 
 const rootReducer = combineSlices({
   cart: cartSlice.reducer,
   favorites: favoritesSlice.reducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
