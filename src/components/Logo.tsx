7ab0ab52224037ad72225bdbd8569faf98892f9b
Andrii Yanchuk <horeca288@gmail.com>
2025-01-15 19:34:46 +0200

    Fix logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export const Logo = () => {
   return (
     <Link to="/">
-      <img className="px-6" src="/img/logo.png" alt="Logo" />
+      <img className="px-6 h-8" src="/img/logo.png" alt="Logo" />
     </Link>
   );
 };
