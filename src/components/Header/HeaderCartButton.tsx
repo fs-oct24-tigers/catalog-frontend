21e564a5db9966d6de7abb6237e69d3c69c43ba5
Andrii Yanchuk <horeca288@gmail.com>
2025-01-15 10:22:12 +0200

    Create badge on Cart Button

--- src/components/Header/HeaderCartButton.tsx
+++ src/components/Header/HeaderCartButton.tsx
@@ -13,7 +13,7 @@ export const HeaderCartButton: React.FC<Props> = ({ handleCloseMenu }) => {
       className="flex grow justify-center"
     >
       <div className="flex items-center justify-center w-16 h-16 hover:bg-lineGray grow">
-        <div className="absolute flex items-center justify-center top-4 right-4 rounded-full h-[18px] w-[18px] border-2 border-bodyBg bg-red-700 z-10 text-xs leading-none">
+        <div className="absolute flex items-center justify-center top-4 right-4 rounded-full h-[18px] w-[18px] border-[1px] border-bodyBg hover:border- bg-red-700 z-10 text-[9px] leading-none">
           12
         </div>
         <ShoppingBag size={20} className="relative" />
