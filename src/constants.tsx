3504e86086ed5c4449f414ab897e6ea9bc8703e2
Alyona Verbanova <alyona.verbanova22@gmail.com>
2025-01-15 19:34:02 +0100

    colors fix

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,5 @@
 import { Color } from './types';
+import { Product } from './types';
 
 export const colorOptions: Record<Color, string> = {
   green: '#51E5BD',
@@ -9,16 +10,39 @@ export const colorOptions: Record<Color, string> = {
   purple: '#7B68EE',
   spacegray: '#3D3D3D',
   midnightgreen: '#008000',
-  gold: '#CD853F',
-  silver: '#808080',
-  rosegold: '#EE716A',
-  coral: '#FFA500',
+  gold: '#ffdab9',
+  silver: '#C1C1C1',
+  rosegold: '#f799bb',
+  coral: '#e6b8c7',
   midnight: '#008080',
   spaceblack: '#2F4F4F',
-  blue: '#00BFFF',
-  pink: '#FA9BCB',
+  blue: '#7ab4ea',
+  pink: '#db7093',
   graphite: '#5B5B5B',
   sierrablue: '#4682B4',
   starlight: '#FFFAE5',
   skyblue: '#5FABFF',
+};
+
+export const getSpecs = (product: Product) => {
+  if (!product) return [];
+  return [
+    { name: 'Screen', value: product.screen },
+    { name: 'Resolution', value: product.resolution },
+    { name: 'Processor', value: product.processor },
+    { name: 'RAM', value: product.ram },
+    { name: 'Camera', value: product.camera },
+    { name: 'Zoom', value: product.zoom },
+    { name: 'Cell', value: product.cell?.join(', ') },
+  ];
+};
+
+export const getProperties = (product: Product) => {
+  if (!product) return [];
+  return [
+    { name: 'Screen', value: product.screen },
+    { name: 'Resolution', value: product.resolution },
+    { name: 'Processor', value: product.processor },
+    { name: 'RAM', value: product.ram },
+  ];
 };
