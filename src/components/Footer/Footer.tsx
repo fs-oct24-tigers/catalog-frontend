e9a907e0c4a1cf6b7632985ecd0e3be0e040ae5d
Dmytro <fridrifd3554@gmail.com>
2025-01-17 23:04:02 +0200

    removed assets folder and index.ts from 404 page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '@/assets/logo.png';
+import logo from '@/../public/img/logo.png';
 import { Link } from 'react-router-dom';
 import { ChevronUp } from 'lucide-react';
 import '@/css/index.css';
